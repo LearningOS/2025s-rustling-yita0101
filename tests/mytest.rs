41a137c65dc608c45b05ba7fb65289a8313c7ff2
arrake <arrake@163.com>
2025-04-03 23:00:41 +0800

    up

--- tests/mytest.rs
+++ tests/mytest.rs
@@ -1,10 +1,17 @@
 
+
+
+use std::thread;
+use std::time::Duration;
+
 #[test]
 pub fn demo01(){
-    let word = "hello";
-    println!("{:?}", get_first_char(word)); // 输出: Some('h')
+    let v = vec![1, 2, 3];
 
-    fn get_first_char(s: &str) -> Option<char> {
-        let first = s.chars().next()?; // `?` 解包 Some(c)，返回 `c`
-        Some(first)
-    }}
+    let handle = thread::spawn(|| {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
+
+}
