476b719c98f5362e057fbc4a8f36c1622f2b7330
arrake <arrake@163.com>
2025-04-04 17:27:46 +0800

    up

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,24 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+
+        let arr:Vec<&str>= s.split(",").collect();
+
+        if arr.len() ==2{
+            let name = arr[0].trim().to_string();
+            if name.is_empty() {
+              return  Person::default()
+            }
+            let age = arr[1].trim().parse::<usize>();
+            if let Ok(age)=age{
+                return Person { name, age };
+            }
+        }
+        Person::default()
+
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty)
+        }
+        let arr:Vec<&str> = s.split(",").collect();
+        if arr.len()==2 {
+            let name = arr[0].trim().to_string();
+            if name.is_empty() {
+                return Err(ParsePersonError::NoName)
+            }
+            let age = arr[1].trim().parse::<usize>();
+            return  match age {
+                Ok(age)=> Ok(Person{name,age}),
+                Err(e)=>Err(ParsePersonError::ParseInt(e))
+            }
+        }
+        Err(ParsePersonError::BadLen)
     }
 }
 
